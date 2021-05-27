1cabe720c70d8713e67119ac1087841d31c0a553
maykaltenev <michaeltenev@gmail.com>
2021-05-27 13:33:38 +0200

    JS Advanced

---
+++ JS Advanced/Lab/P06_SumOfNumberN...M.js
@@ -0,0 +1,12 @@
+function sumOfNumbers(n, m) {
+
+    let start = Number(n);
+    let end = Number(m);
+
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    console.log(sum);
+}
+sumOfNumbers('1', '5');
