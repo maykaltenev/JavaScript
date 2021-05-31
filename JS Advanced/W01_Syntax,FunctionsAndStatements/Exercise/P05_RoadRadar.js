d07b8da51d6715da444be41bc21fe6bbcd56dd85
maykaltenev <michaeltenev@gmail.com>
2021-05-31 19:22:13 +0200

    Week 1

---
+++ JS Advanced/W01_Syntax,FunctionsAndStatements/Exercise/P05_RoadRadar.js
@@ -0,0 +1,19 @@
+function roadRadar(speed, type) {
+    let citySpeed = {
+        'city': 50,
+        'motorway': 130,
+        'residential': 20,
+        'interstate': 90
+    }
+    speed = Number(speed);
+    let diff = speed - citySpeed[type];
+    if (citySpeed[type] >= speed && speed > 0) {
+        console.log(`Driving ${speed} km/h in a ${citySpeed[type]} zone`)
+    } else if (citySpeed[type] + 20 >= speed) {
+        console.log(`The speed is ${diff} km/h faster than the allowed speed of ${citySpeed[type]} - speeding`);
+    } else if (citySpeed[type] + 40 >= speed) {
+        console.log(`The speed is ${diff} km/h faster than the allowed speed of ${citySpeed[type]} - excessive speeding`);
+    } else {
+        console.log(`The speed is ${diff} km/h faster than the allowed speed of ${citySpeed[type]} - reckless driving`)
+    }
+}
